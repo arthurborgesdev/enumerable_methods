7a0dba93abc6a1aed1567f63df4a529d2d476c9c
Arthur Borges <arthurborges.dev@gmail.com>
2021-04-05 16:57:13 -0300

    Add three test cases to my_each method for Hashes

--- spec/enumerable_methods_spec.rb
+++ spec/enumerable_methods_spec.rb
@@ -2,9 +2,26 @@ require_relative '../enumerable_methods'
 
 describe Enumerable do
   describe '#my_each' do
+    let(:a)  { ["a", "b", "c"] }
     it "prints each element of the array separated by ' -- '" do
-      a = ["a", "b", "c"]
       expect { a.my_each { |x| print x, " -- "} }.to output("a -- b -- c -- ").to_stdout
+    end
+    it "returns the array which was called upon" do
+      expect(a.my_each { |x| print x, " -- "}).to be == a
+    end
+    it "returns an Enumerator if no block is passed for Array" do
+      expect(a.my_each).to be_a(Enumerator)
+    end
+
+    h = {"a" => 100, "b" => 200 }
+    it "puts each key/value pair of the hash in the format 'key is value'" do
+      expect { h.my_each { |key, value| puts "#{key} is #{value}" } }.to output("a is 100\nb is 200\n").to_stdout
+    end
+    it "returns the hash which was called upon" do
+      expect(h.my_each { |key, value| puts "#{key} is #{value}" }).to be == h
+    end
+    it "returns an Enumerator if no block is passed for Hash" do
+      expect(h.my_each).to be_a(Enumerator)
     end
   end
 end
